41580455b2b61676f5ea2e86589cca79d81af83c
Mary <git@mary.my.id>
2024-12-26 17:48:49 +0700

    refactor(bluesky-threading): upgrade cid dependency

--- packages/bluesky/threading/lib/cbor.ts
+++ packages/bluesky/threading/lib/cbor.ts
@@ -1,5 +1,5 @@
 import { encode } from '@atcute/cbor';
-import { create, format } from '@atcute/cid';
+import { create, toString } from '@atcute/cid';
 
 // Sanity-check by requiring a $type here, this is because the records are
 // expected to be encoded with it, even though the PDS accepts record writes
@@ -8,7 +8,7 @@ export async function serializeRecordCid(record: { $type: string }): Promise<str
 	const bytes = encode(record);
 
 	const cid = await create(0x71, bytes);
-	const serialized = format(cid);
+	const serialized = toString(cid);
 
 	return serialized;
 }
